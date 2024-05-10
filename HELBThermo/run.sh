a20299833212b6cb6d27c175c80ae91d6ed98c1e
Takutotfu <123989660+Takutotfu@users.noreply.github.com>
2024-05-10 13:52:55 +0200

    Revert "Update run.sh"
    This reverts commit bb04f6d09c219fe26a3de053c00aaa7c7e166614.

--- HELBThermo/run.sh
+++ HELBThermo/run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-mvn clean package
-java -jar --module-path /usr/share/openjfx/lib --add-modules=javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.media,javafx.swing,javafx.web target/HELBThermo-1.0-SNAPSHOT.jar
 
+mvn clean package
+
+if [ $? -eq 0 ]; then
+	java -jar --module-path /usr/share/openjfx/lib --add-modules=javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.media,javafx.swing,javafx.web target/HELBThermo-1.0-SNAPSHOT.jar
+else
+	echo "Error build"
+fi
