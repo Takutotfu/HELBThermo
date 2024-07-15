2789dfa2b305010cb71a43f25969e6416563c21c
lenny.solt <123989660+Takutotfu@users.noreply.github.com>
2024-07-15 21:58:21 +0200

    Update run.bat

--- HELBThermo/run.bat
+++ HELBThermo/run.bat
@@ -1,10 +1,11 @@
 @echo off
 
+set PATH_TO_FX = "C:\path\to\javafx\lib"
+
 if not exist target\HELBThermo-1.0-SNAPSHOT.jar (
 	mvn clean package
 )
 
-java -jar --module-path "C:\Program Files\Java\javafx-sdk\lib" --add-modules=javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.media,javafx.swing,javafx.web target\HELBThermo-1.0-SNAPSHOT.jar
-
+java -jar --module-path "%PATH_TO_FX%" --add-modules=javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.media,javafx.swing,javafx.web target\HELBThermo-1.0-SNAPSHOT.jar
 
 pause
